b2d2992a664770295edf7138d2981fccca55a221
Shamash2014 <warp.buddhist@gmail.com>
2020-05-17 18:27:36 +0300

    feat: Add hook to receive data

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-import { NativeModules } from 'react-native';
+import { NativeModules } from "react-native";
 
 const { ShareReceiver } = NativeModules;
+const eventEmitter = new NativeEventEmitter(ShareReceiver);
+
+export const useShareData = () => {
+  const [state, setState] = useState(null);
+
+  useEffect(() => {
+    const eventListener = eventEmitter.addListener("intent", (event) => {
+      console.log(event.eventProperty); // "someValue"
+
+      setState(event);
+    });
+
+    return () => {
+      eventListener.remove();
+    };
+  });
+  return state;
+};
 
 export default ShareReceiver;
