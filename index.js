98ccef72d598b51a270d6fe813e25957c667be9a
Shamash2014 <warp.buddhist@gmail.com>
2020-05-17 18:32:22 +0300

    fix: Fix dependency

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { NativeModules } from "react-native";
-
+import { NativeModules, NativeEventEmitter } from "react-native";
 const { ShareReceiver } = NativeModules;
 const eventEmitter = new NativeEventEmitter(ShareReceiver);
 
